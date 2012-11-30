a7e2fa1bb44ded529dc0374b12a56188787b298d
Nic Barker <bicnarker@me.com>
2012-11-30 15:17:28 +1100

    Added a redirect from /home to /profile if logged in.

--- src/Qiiss/GeneralBundle/Controller/GeneralController.php
+++ src/Qiiss/GeneralBundle/Controller/GeneralController.php
@@ -3,13 +3,23 @@
 namespace Qiiss\GeneralBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class GeneralController extends Controller
 {
     public function indexAction()
     {
-				$token = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
-        return $this->render('QiissGeneralBundle:Default:splash.html.twig', array('token' => $token));
+    	$container = $this->container;
+		if( $container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ){
+		    $url = $this->get('router')->generate('fos_user_profile_show');
+		    return new RedirectResponse($url);
+		}
+		else {
+			$token = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
+        	return $this->render('QiissGeneralBundle:Default:splash.html.twig', array('token' => $token));
+    	}
     }
 
 		public function getTokenAction()
