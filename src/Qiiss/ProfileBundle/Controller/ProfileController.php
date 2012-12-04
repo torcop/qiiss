a337b2d66757b4b934c275fbbbd2ef4f176b631f
Nic Barker <bicnarker@me.com>
2012-12-04 12:48:23 +1100

    Fixed up some conflicts

--- src/Qiiss/ProfileBundle/Controller/ProfileController.php
+++ src/Qiiss/ProfileBundle/Controller/ProfileController.php
@@ -9,43 +9,28 @@ use Qiiss\ProfileBundle\Form\DateType;;
 
 class ProfileController extends Controller
 {
-    public function proposeDateAction()
-    {
-			$date = new Date();
-			$form = $this->createForm(new DateType, $date);
-			$noty = new Noty();
-			$noty->setDate(new \Datetime());
-    	$noty->setSender(7);
-			$noty->setTarget(3);
-			$noty->setType('New date');
-			$noty->setContent('Nizar request a new date for you!');
-			$noty->setLink('yeah.com.au');
+  public function proposeDateAction() {
+    $date = new Date();
+    $form = $this->createForm(new DateType, $date);
+    $noty = new Noty();
+    $noty->setDate(new \Datetime());
+    $noty->setSender(7);
+    $noty->setTarget(3);
+    $noty->setType('New date');
+    $noty->setContent('Nizar request a new date for you!');
+    $noty->setLink('yeah.com.au');
 
-<<<<<<< HEAD
-			$request = $this->get('request');
-			if( $request->getMethod() == 'POST' )
-			{
-				$form->bind($request);
-				if($form->isValid())
-				{
-					$em = $this->getDoctrine()->getEntityManager();
+    $request = $this->get('request');
+    if( $request->getMethod() == 'POST' ) {
+      $form->bind($request);
+      if($form->isValid()) {
+          $em = $this->getDoctrine()->getEntityManager();
           $em->persist($date);
-					$em->persist($noty);
+          $em->persist($noty);
           $em->flush();
           return $this->redirect( $this->generateUrl('qiiss_general_homepage') );
-=======
-		$request = $this->get('request');
-		if( $request->getMethod() == 'POST' ) {
-        	$form->bind($request);
-        	if($form->isValid()) {
-				$em = $this->getDoctrine()->getEntityManager();
-                $em->persist($date);
-                $em->flush();
-                return $this->redirect( $this->generateUrl('qiiss_general_homepage') );
-            }
->>>>>>> a84561f0ed837f6fa6c9bc578794205f9fc35279
-        }
-    	}
-			return $this->render('QiissProfileBundle:Profile:date.html.twig', array("form" => $form->createView()));
-		}
+      }
+    }
+    return $this->render('QiissProfileBundle:Profile:date.html.twig', array("form" => $form->createView()));
+  }
 }
