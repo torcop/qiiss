d36a737456b8e7b73576867209b64808f9472f26
Nic Barker <bicnarker@me.com>
2012-12-04 18:59:32 +1100

    Fixed up some more stuff on the propose a date front end

---
+++ src/Qiiss/ProfileBundle/Resources/public/js/proposeDate.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.content_header').bind('click', function() {
+		var body = $(this).parent().find('.content_block');
+		if (body.css('display') == "none") {
+			body.slideDown(280);
+		}
+		else {
+			body.slideUp(280);
+		}
+	});
+
+	$('.header').bind('click', function() {
+		var body = $('.content_block');
+		if (body.css('display') == "none") {
+			body.slideDown(280);
+		}
+		else {
+			body.slideUp(280);
+		}
+	});
+});
