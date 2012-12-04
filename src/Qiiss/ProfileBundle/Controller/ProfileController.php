dfab9d07301c6f39643da6e90b3eef46e9614325
mz-hacker <mz-hacker@mzhacker-K53SD.(none)>
2012-12-04 12:50:00 +1100

    Modificationinside ProfileController.php

--- src/Qiiss/ProfileBundle/Controller/ProfileController.php
+++ src/Qiiss/ProfileBundle/Controller/ProfileController.php
@@ -5,7 +5,7 @@ namespace Qiiss\ProfileBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Qiiss\ProfileBundle\Entity\Date;
 use Qiiss\NotyBundle\Entity\Noty;
-use Qiiss\ProfileBundle\Form\DateType;;
+use Qiiss\ProfileBundle\Form\DateType;
 
 class ProfileController extends Controller
 {
@@ -21,7 +21,6 @@ class ProfileController extends Controller
 			$noty->setContent('Nizar request a new date for you!');
 			$noty->setLink('yeah.com.au');
 
-<<<<<<< HEAD
 			$request = $this->get('request');
 			if( $request->getMethod() == 'POST' )
 			{
@@ -33,17 +32,6 @@ class ProfileController extends Controller
 					$em->persist($noty);
           $em->flush();
           return $this->redirect( $this->generateUrl('qiiss_general_homepage') );
-=======
-		$request = $this->get('request');
-		if( $request->getMethod() == 'POST' ) {
-        	$form->bind($request);
-        	if($form->isValid()) {
-				$em = $this->getDoctrine()->getEntityManager();
-                $em->persist($date);
-                $em->flush();
-                return $this->redirect( $this->generateUrl('qiiss_general_homepage') );
-            }
->>>>>>> a84561f0ed837f6fa6c9bc578794205f9fc35279
         }
     	}
 			return $this->render('QiissProfileBundle:Profile:date.html.twig', array("form" => $form->createView()));
