a9c530efa48342fa666a1c30fc9a65c5f229ee10
Nic Barker <bicnarker@me.com>
2012-12-05 11:01:27 +1100

    added click events to close the notification boxes, styled the calender in propose a date a little more

--- src/Qiiss/UserBundle/Resources/public/js/header.js
+++ src/Qiiss/UserBundle/Resources/public/js/header.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".notifications").bind("click", function(e) {
+  $(".notifications").bind("click", function() {
     var popup = $("#" + $(this).attr('id').split("_")[0] + "_popup");
     if (popup.css("display") != "none") {
       popup.slideUp(300);
@@ -18,11 +18,14 @@ $(document).ready(function() {
     });
   });
 
-  $("#")
+  $("#popup_item").bind("click", function() {
 
-  $("#body").bind("click", function() {
+  });
+
+  $("body").bind("click", function(event) {
     $(".popup").each(function() {
-      if ($(this).css("display") != "none" && !$(this).hasClass("animating")) {
+      if ($(this).css("display") != "none" && !$(this).hasClass("animating") && $(event.target).context.className.indexOf("popup") == -1) {
+        console.log($(event.target).context.className);
         $(this).slideUp(300);
       return;
       }
