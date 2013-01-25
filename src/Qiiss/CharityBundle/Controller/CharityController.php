5c99473451b41079687128bcc10884c5884ababe
mz-hacker <mz-hacker@mzhacker-K53SD.(none)>
2013-01-25 15:31:59 +1100

    [CharityBundle] We can add the charity inside our database.

--- src/Qiiss/CharityBundle/Controller/CharityController.php
+++ src/Qiiss/CharityBundle/Controller/CharityController.php
@@ -4,6 +4,7 @@ namespace Qiiss\CharityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Qiiss\CharityBundle\Entity\Charity;
+use Qiiss\CharityBundle\Form\CharityType;
 
 class CharityController extends Controller
 {
@@ -12,7 +13,20 @@ class CharityController extends Controller
     {
 				$charity = new Charity();
 
+				$form = $this->createForm(new CharityType, $charity);
+				$request = $this->get('request');
+				
+				if ($request->getMethod() == 'POST')
+				{
+					$form->bind($request);
+					if ($form->isValid())
+					{
+						$em = $this->getDoctrine()->getEntityManager();
+						$em->persist($charity);
+						$em->flush();
+					}
+				}
 
-        return $this->render('QiissCharityBundle:Default:charity.html.twig');
+        return $this->render('QiissCharityBundle:Default:charity.html.twig', array('form' => $form->createView()));
     }
 }
